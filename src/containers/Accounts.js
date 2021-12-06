70490e4cd3f68cb2672a803a9bc2a57d514ec283
Gabriel Ortolaza <gabriel.ortolaza@toptal.com>
2021-12-06 03:35:17 -0500

    generate common components

--- src/containers/Accounts.js
+++ src/containers/Accounts.js
@@ -4,105 +4,24 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import SwipeableViews from 'react-swipeable-views';
 import Container from '@mui/material/Container';
-import TextField from '@mui/material/TextField';
 import { useTheme } from '@mui/material/styles';
-import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
 
+import AutoComplete from '../components/AutoComplete';
+import TabPanel from '../components/TabPanel';
 import AccountsTable from './AccountsTable';
-
-const filter = createFilterOptions();
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`full-width-tabpanel-${index}`}
-      aria-labelledby={`full-width-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          {children}
-        </Box>
-      )}
-    </div>
-  );
-}
 
 function Accounts() {
   const theme = useTheme();
-  const [value, setValue] = React.useState(null);
   const [currentTab, setCurrentTab] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setCurrentTab(newValue);
   };
 
-  const handleChangeIndex = (index) => {
-    setValue(index);
-  };
-
   return (
     <Container maxWidth="lg">
       <Box sx={{ mt: 8 }}>
-        <Autocomplete
-          value={value}
-          onChange={(event, newValue) => {
-            if (typeof newValue === 'string') {
-              setValue({
-                name: newValue,
-              });
-            } else if (newValue && newValue.inputValue) {
-              // Create a new value from the user input
-              setValue({
-                name: newValue.inputValue,
-              });
-            } else {
-              setValue(newValue);
-            }
-          }}
-          filterOptions={(options, params) => {
-            const filtered = filter(options, params);
-
-            const { inputValue } = params;
-            // Suggest the creation of a new value
-            const isExisting = options.some((option) => inputValue === option.name);
-            if (inputValue !== '' && !isExisting) {
-              filtered.push({
-                inputValue,
-                name: `Add "${inputValue}"`,
-              });
-            }
-
-            return filtered;
-          }}
-          selectOnFocus
-          clearOnBlur
-          handleHomeEndKeys
-          id="free-solo-with-text-demo"
-          options={usersData}
-          getOptionLabel={(option) => {
-            // Value selected with enter, right from the input
-            if (typeof option === 'string') {
-              return option;
-            }
-            // Add "xxx" option created dynamically
-            if (option.inputValue) {
-              return option.inputValue;
-            }
-            // Regular option
-            return option.name;
-          }}
-          renderOption={(props, option) => <li {...props}>{option.name}</li>}
-          sx={{ width: 300 }}
-          freeSolo
-          renderInput={(params) => (
-            <TextField {...params} label="Search accounts" />
-          )}
-        />
+        <AutoComplete />
       </Box>
       <Box sx={{ mt: 8 }}>
         <Tabs
@@ -124,7 +43,7 @@ function Accounts() {
       <SwipeableViews
         axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
         index={currentTab}
-        onChangeIndex={handleChangeIndex}
+        onChangeIndex={handleChange}
       >
         <TabPanel value={currentTab} index={0} dir={theme.direction}>
           <AccountsTable />
@@ -136,13 +55,5 @@ function Accounts() {
     </Container>
   );
 }
-
-const usersData = [
-  { name: 'Jane Wilson', role: 'Admin' },
-  { name: 'Kevin Ryder', role: 'Editor' },
-  { name: 'Matthew Gas', role: 'Editor' },
-  { name: 'Erik Bahena', role: 'Admin' },
-  { name: 'John Doe', role: 'Admin' },
-]
 
 export default Accounts;
