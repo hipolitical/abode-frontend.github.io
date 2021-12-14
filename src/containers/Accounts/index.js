b57e9f568f1b05e1822bc3cf6ee2a380916a71c5
Gabriel Ortolaza <gabriel.ortolaza@toptal.com>
2021-12-14 02:34:38 -0500

    Disable old Accounts tab

--- src/containers/Accounts/index.js
+++ src/containers/Accounts/index.js
@@ -6,16 +6,12 @@ import Tab from '@mui/material/Tab';
 import SwipeableViews from 'react-swipeable-views';
 import Container from '@mui/material/Container';
 import { useTheme } from '@mui/material/styles';
-import Button from '@mui/material/Button';
 
 import AutoComplete from '../../components/AutoComplete';
 import TabPanel from '../../components/TabPanel';
 import TableList from '../../components/TableList';
-import AddEditModal from './modal';
 import {
   getMyClients,
-  addClientAccount,
-  updateClientAccount,
 } from '../../store/actions/accounts'
 import { getAllAccounts } from '../../store/actions/all_accounts'
 import { getRequests } from '../../store/actions/requests'
@@ -24,9 +20,6 @@ function Accounts() {
   const theme = useTheme();
   const dispatch = useDispatch();
   const [currentTab, setCurrentTab] = React.useState(0);
-  const [currentAccount, setCurrentAccount] = React.useState(null);
-  const [openAccountModal, setOpenAccountModal] = React.useState(false);
-  const accountsData = useSelector(state => state.accounts);
   const allAccountsData = useSelector(state => state.all_accounts);
   const requestsData = useSelector(state => state.requests);
 
@@ -43,34 +36,11 @@ function Accounts() {
     }
   };
 
-  const handleClickOpenAccountModal = () => {
-    setOpenAccountModal(true);
-  };
-
-  const handleCloseAccountModal = () => {
-    setOpenAccountModal(false);
-    setCurrentAccount(null);
-  };
-
-  const handleAccountInfoSubmit = (values) => {
-    if (values.id || currentAccount) {
-      dispatch(updateClientAccount(values));
-    } else {
-      dispatch(addClientAccount(values));
-    }
-    handleCloseAccountModal()
-  }
-
-  const handleOpenEdit = (rowData) => {
-    setCurrentAccount(rowData);
-    setOpenAccountModal(true);
-  }
-
   return (
     <Container maxWidth="lg">
       <Box sx={{ mt: 8 }}>
-        {accountsData.accounts && (
-          <AutoComplete suggestions={accountsData.accounts} />
+        {allAccountsData.accounts && (
+          <AutoComplete suggestions={allAccountsData.accounts} />
         )}
       </Box>
       <Box sx={{ mt: 8 }}>
@@ -101,22 +71,10 @@ function Accounts() {
         onChangeIndex={handleChange}
       >
         <TabPanel value={currentTab} index={0} dir={theme.direction}>
-          <Box sx={{ mb: 2 }}>
-            <Button variant="outlined" onClick={handleClickOpenAccountModal}>
-              Add a new client
-            </Button>
-          </Box>
           <TableList
-            rows={accountsData.accounts}
-            headers={accountsData.headers}
-            type="editing"
-            openEdit={handleOpenEdit}
-          />
-          <AddEditModal
-            open={openAccountModal}
-            initData={currentAccount}
-            handleClose={handleCloseAccountModal}
-            handleSubmit={handleAccountInfoSubmit}
+            rows={allAccountsData.accounts}
+            headers={allAccountsData.headers}
+            type="access"
           />
         </TabPanel>
         <TabPanel value={currentTab} index={1} dir={theme.direction}>
