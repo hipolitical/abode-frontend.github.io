712c9e643cd913cd4c1e3e599c1b3772c6cd57fa
Gabriel Ortolaza <gabriel.ortolaza@toptal.com>
2021-12-17 12:33:48 -0500

    trim navbar content

--- src/containers/Navbar/index.js
+++ src/containers/Navbar/index.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -8,14 +8,11 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import { NavLink } from 'react-router-dom';
 import LogoImg from '../../assets/logo.svg';
 import NotesIcon from '../../assets/notes.svg';
 import AlertsIcon from '../../assets/alerts.svg';
-import AvatarImg from '../../assets/avatar.png';
 import './navbar.css';
 
 const pages = [
@@ -27,29 +24,14 @@ const blockedRoutes = ['/login']
 
 const Navbar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
   const location = useLocation();
-  const navigate = useNavigate();
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
-  const handleLogout = () => {
-    handleCloseUserMenu();
-    localStorage.clear();
-    navigate('/login');
   };
 
   if (blockedRoutes.includes(location.pathname)) {
@@ -139,7 +121,7 @@ const Navbar = () => {
             ))}
           </Box>
 
-          <Box sx={{ flexGrow: 0 , display: { xs: 'none', md: 'flex', marginRight: 60 }}}>
+          <Box sx={{ flexGrow: 0 , display: { xs: 'none', md: 'flex' }}}>
             <MenuItem className="nav-icon">
               <img src={NotesIcon} alt="notes" />
             </MenuItem>
@@ -147,37 +129,7 @@ const Navbar = () => {
               <img src={AlertsIcon} alt="alerts" />
             </MenuItem>
           </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src={AvatarImg} />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              <MenuItem onClick={handleCloseUserMenu}>
-                <Typography textAlign="center">Profile</Typography>
-              </MenuItem>
-              <MenuItem onClick={handleLogout}>
-                <Typography textAlign="center">Logout</Typography>
-              </MenuItem>
-            </Menu>
-          </Box>
+          
         </Toolbar>
       </Container>
     </AppBar>
