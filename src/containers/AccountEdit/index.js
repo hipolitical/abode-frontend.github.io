f187bb43e034ad9a04c6e3c0682aba69703221f9
Gabriel Ortolaza <gabriel.ortolaza@toptal.com>
2021-12-21 15:22:11 -0500

    navigate back process for account edit page

--- src/containers/AccountEdit/index.js
+++ src/containers/AccountEdit/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-
+import Button from '@mui/material/Button';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import TableList from '../../components/TableList';
 import { getMyAccounts } from '../../store/actions/my_accounts';
 import { getCurrentUserId } from '../../utils/functions';
 
 function AccountEdit() {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const accountsData = useSelector(state => state.accounts);
 
@@ -22,6 +25,13 @@ function AccountEdit() {
         <Typography variant="h4" color="primary" gutterBottom>
           Markel Insurnace Company
         </Typography>
+        <Button
+          variant="outlined"
+          startIcon={<ArrowBackIcon />}
+          onClick={() => navigate(-1)}
+        >
+          Back
+        </Button>
       </Box>
       <Box sx={{ mt: 4 }}>
         <TableList
