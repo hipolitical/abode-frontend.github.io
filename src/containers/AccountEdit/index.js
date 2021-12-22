6c761dd2ce0fd8585ed8cb72745298a327d35c98
Gabriel Ortolaza <gabriel.ortolaza@toptal.com>
2021-12-22 12:32:04 -0500

    connect edit screen with single account api

--- src/containers/AccountEdit/index.js
+++ src/containers/AccountEdit/index.js
@@ -1,29 +1,44 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import CircularProgress from '@mui/material/CircularProgress';
 import TableList from '../../components/TableList';
 import { getMyAccounts } from '../../store/actions/my_accounts';
+import { getSingleAccount } from '../../store/actions/single_account';
 import { getCurrentUserId } from '../../utils/functions';
 
 function AccountEdit() {
+  const { id } = useParams();
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const accountsData = useSelector(state => state.accounts);
+  const singleAccountData = useSelector(state => state.single_account);
+  const displayName = singleAccountData.single_account?.display_name;
+  const isLoading = singleAccountData.isLoading;
 
   React.useEffect(() => {
     dispatch(getMyAccounts(getCurrentUserId()));
-  }, [dispatch]);
+    dispatch(getSingleAccount(id));
+  }, [dispatch, id]);
+
+  if (isLoading) {
+    return (
+      <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 8 }}>
+        <CircularProgress />
+      </Box>
+    )
+  }
 
   return (
     <Container maxWidth="lg">
       <Box sx={{ mt: 6 }}>
         <Typography variant="h4" color="primary" gutterBottom>
-          Markel Insurnace Company
+          {displayName}
         </Typography>
         <Button
           variant="outlined"
