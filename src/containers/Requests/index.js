a61b3f9df9eb7959aa05ba60e85eef92e68a08a0
Gabriel Ortolaza <gabriel.ortolaza@toptal.com>
2022-01-11 10:53:47 -0500

    grant action delay and undo process

--- src/containers/Requests/index.js
+++ src/containers/Requests/index.js
@@ -8,22 +8,27 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import Typography from '@mui/material/Typography';
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import TableList from '../../components/TableList';
 import SearchInput from '../../components/SearchInput';
 import { getRequests, grantAccess } from '../../store/actions/requests';
 import { getCurrentUserId, getCurrentUserType } from '../../utils/functions';
 import { filterBySearchKeyword } from '../../utils/helpers';
+import './style.css';
 
 function Requests() {
   const dispatch = useDispatch();
   const accountsData = useSelector(state => state.requests);
-  const [searchKeyword, setSearchKeyword] = useState('')
-  const [pageSize, setPageSize] = useState(10)
-  const [pageNumber, setPageNumber] = useState(0)
-  const availableData = filterBySearchKeyword(accountsData.requests, searchKeyword)
-  const [openDeclineModal, setOpenDeclineModal] = useState(false)
-  const [openGrantModal, setOpenGrantModal] = useState(false)
-  const [currentParams, setCurrentParams] = useState({})
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const [pageSize, setPageSize] = useState(10);
+  const [pageNumber, setPageNumber] = useState(0);
+  const availableData = filterBySearchKeyword(accountsData.requests, searchKeyword);
+  const [openDeclineModal, setOpenDeclineModal] = useState(false);
+  const [openGrantModal, setOpenGrantModal] = useState(false);
+  const [currentParams, setCurrentParams] = useState({});
+  const [grantingStatus, setGrantingStatus] = useState(0);
+  const UNDO_LIMIT = 10;
 
   React.useEffect(() => {
     dispatch(getRequests({
@@ -44,6 +49,7 @@ function Requests() {
 
   const handleCloseGrantModal = () => {
     setOpenGrantModal(false);
+    setGrantingStatus(0);
   };
 
   const handleOpenGrantModal = (params) => {
@@ -52,13 +58,30 @@ function Requests() {
   }
 
   const handleGrantAction = () => {
-    if (currentParams) {
+    setGrantingStatus(grantingStatus + 1);
+    if (grantingStatus === 1 && currentParams) {
       dispatch(grantAccess({
         requesterId: 1213311,
         ...currentParams,
       }));
     }
   }
+
+  const handleUndoGrantAction = () => {
+    setGrantingStatus(0);
+  }
+
+  const renderTime = ({ remainingTime }) => {
+    if (remainingTime === 0) {
+      return <div className="timer">Too lale...</div>;
+    }
+  
+    return (
+      <div className="timer">
+        <div className="value">{remainingTime}</div>
+      </div>
+    );
+  };
 
   return (
     <Container maxWidth="lg">
@@ -88,7 +111,12 @@ function Requests() {
         <DialogTitle>Declining Request Modal</DialogTitle>
         <DialogContent dividers>
           <DialogContentText>
-            <div dangerouslySetInnerHTML={{ __html: `Are you sure you want to decline access to <b>${currentParams.requesterName}</b> to <br/><b>${currentParams.targetName}</b>?` }} />
+            <div
+              dangerouslySetInnerHTML={{
+                __html:
+                  `Are you sure you want to decline access to <b>${currentParams.requesterName}</b> to <br/><b>${currentParams.targetName}</b>?`
+              }}
+            />
           </DialogContentText>
         </DialogContent>
         <DialogActions>
@@ -99,13 +127,47 @@ function Requests() {
       <Dialog open={openGrantModal} onClose={handleCloseGrantModal} fullWidth={true} maxWidth={'sm'}>
         <DialogTitle>Granting Request Modal</DialogTitle>
         <DialogContent dividers>
-          <DialogContentText>
-            <div dangerouslySetInnerHTML={{ __html: `Are you sure you want to grant access to <b>${currentParams.requesterName}</b> to <br/><b>${currentParams.targetName}</b>?` }} />
-          </DialogContentText>
+          {grantingStatus === 0 &&
+            <div
+              dangerouslySetInnerHTML={{
+              __html:
+                `Are you sure you want to grant access to <b>${currentParams.requesterName}</b> to <br/><b>${currentParams.targetName}</b>?`
+              }}
+              style={{ lineHeight: '1.5rem' }}
+            />
+          }
+          {grantingStatus === 1 &&
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <CountdownCircleTimer
+                size={60}
+                strokeWidth={6}
+                isPlaying={grantingStatus === 1}
+                duration={UNDO_LIMIT}
+                colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
+                onComplete={handleGrantAction}
+              >
+                {renderTime}
+              </CountdownCircleTimer>
+              <Typography variant="body" sx={{ ml: 3 }}>
+                You can undo this request in the remaining time.
+              </Typography>
+            </Box>
+          }
+          {grantingStatus === 2 &&
+            <div dangerouslySetInnerHTML={{
+              __html:
+                `The request has been sent.<br/><b>${currentParams.requesterName}</b> to <b>${currentParams.targetName}</b>`
+              }}
+              style={{ lineHeight: '1.5rem' }}
+            />
+          }
         </DialogContent>
         <DialogActions>
           <Button onClick={handleCloseGrantModal}>Cancel</Button>
-          <Button type="submit" onClick={handleGrantAction}>Ok</Button>
+          <Button variant="contained" type="submit" onClick={handleGrantAction} disabled={grantingStatus === 2}>
+            {grantingStatus === 1 ? 'Proceed' : 'Ok'}
+          </Button>
+          <Button variant="contained" type="submit" onClick={handleUndoGrantAction} disabled={grantingStatus !== 1}>Undo</Button>
         </DialogActions>
       </Dialog>
     </Container>
