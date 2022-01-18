0a5470bfa73be2237192f50ab38dd854dc30ee5a
Gabriel Ortolaza <gabriel.ortolaza@toptal.com>
2022-01-18 10:55:14 -0500

    manage redux to handle request creation

--- src/store/actions/all_accounts.js
+++ src/store/actions/all_accounts.js
@@ -1,7 +1,12 @@
 import {
   GET_ALL_ACCOUNTS_REQUESTED,
+  CREATE_REQUEST_REQUESTED,
 } from "../types";
 
 export function getAllAccounts(params) {
   return { type: GET_ALL_ACCOUNTS_REQUESTED, params };
+}
+
+export function createRequest(params) {
+  return { type: CREATE_REQUEST_REQUESTED, params };
 }
--- src/store/saga/all_accounts.js
+++ src/store/saga/all_accounts.js
@@ -3,8 +3,11 @@ import {
   GET_ALL_ACCOUNTS_REQUESTED,
   GET_ALL_ACCOUNTS_FAILED,
   GET_ALL_ACCOUNTS_SUCCESS,
+  CREATE_REQUEST_REQUESTED,
+  CREATE_REQUEST_FAILED,
+  CREATE_REQUEST_SUCCESS,
 } from "../types";
-import { getAllAccounts } from '../../api';
+import { getAllAccounts, createRequest } from '../../api';
 
 function* getAllAccountsRequested({ params }) {
   try {
@@ -15,7 +18,18 @@ function* getAllAccountsRequested({ params }) {
   }
 }
 
+function* createRequestHandler({ params }) {
+  try {
+    const payload = yield call(createRequest, params);
+    yield put({ type: CREATE_REQUEST_SUCCESS, payload });
+  } catch (e) {
+    yield put({ type: CREATE_REQUEST_FAILED, payload: e });
+  }
+}
+
+
 export default function* accounts() {
   yield takeEvery(GET_ALL_ACCOUNTS_REQUESTED, getAllAccountsRequested);
+  yield takeEvery(CREATE_REQUEST_REQUESTED, createRequestHandler);
 }
 
