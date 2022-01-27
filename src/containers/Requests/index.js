f9b25a41795fc65c029a7bf6483f33937da371c5
Gabriel Ortolaza <gabriel.ortolaza@toptal.com>
2022-01-27 15:26:46 -0500

    replace dialog processes with common ActionModal

--- src/containers/Requests/index.js
+++ src/containers/Requests/index.js
@@ -2,13 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
-import Typography from '@mui/material/Typography';
-import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import ActionModal from '../../components/ActionModal';
 import TableList from '../../components/TableList';
 import SearchInput from '../../components/SearchInput';
 import { getRequests, grantAccess, declineAccess } from '../../store/actions/requests';
@@ -27,9 +21,6 @@ function Requests() {
   const [openDeclineModal, setOpenDeclineModal] = useState(false);
   const [openGrantModal, setOpenGrantModal] = useState(false);
   const [currentParams, setCurrentParams] = useState({});
-  const [grantingStatus, setGrantingStatus] = useState(0);
-  const [decliningStatus, setDecliningStatus] = useState(0);
-  const UNDO_LIMIT = 5;
 
   React.useEffect(() => {
     dispatch(getRequests({
@@ -42,7 +33,6 @@ function Requests() {
 
   const handleCloseDeclineModal = () => {
     setOpenDeclineModal(false);
-    setDecliningStatus(0);
   };
 
   const handleOpenDeclineModal = (params) => {
@@ -52,7 +42,6 @@ function Requests() {
 
   const handleCloseGrantModal = () => {
     setOpenGrantModal(false);
-    setGrantingStatus(0);
   };
 
   const handleOpenGrantModal = (params) => {
@@ -61,8 +50,7 @@ function Requests() {
   }
 
   const handleGrantAction = () => {
-    setGrantingStatus(grantingStatus + 1);
-    if (grantingStatus === 1 && currentParams) {
+    if (currentParams) {
       dispatch(grantAccess({
         requesterId: getCurrentUserId(),
         ...currentParams,
@@ -71,34 +59,13 @@ function Requests() {
   }
 
   const handleDeclineAction = () => {
-    setDecliningStatus(decliningStatus + 1);
-    if (decliningStatus === 1 && currentParams) {
+    if (currentParams) {
       dispatch(declineAccess({
         requesterId: getCurrentUserId(),
         ...currentParams,
       }));
     }
   }
-
-  const handleUndoGrantAction = () => {
-    setGrantingStatus(0);
-  }
-
-  const handleUndoDeclineAction = () => {
-    setDecliningStatus(0);
-  }
-
-  const renderTime = ({ remainingTime }) => {
-    if (remainingTime === 0) {
-      return <div className="timer">Too late...</div>;
-    }
-  
-    return (
-      <div className="timer">
-        <div className="value">{remainingTime}</div>
-      </div>
-    );
-  };
 
   return (
     <Container maxWidth="lg">
@@ -124,126 +91,22 @@ function Requests() {
           type={isAdmin() ? "requests" : "my_requests"}
         />
       </Box>
-      <Dialog open={openDeclineModal} onClose={handleCloseDeclineModal} fullWidth={true} maxWidth={'sm'}>
-        <DialogTitle>Declining Request Modal</DialogTitle>
-        <DialogContent dividers>
-          {decliningStatus === 0 &&
-            <div
-              dangerouslySetInnerHTML={{
-              __html:
-                `Are you sure you want to decline access to <b>${currentParams.requesterName}</b> to <br/><b>${currentParams.targetName}</b>?`
-              }}
-              style={{ lineHeight: '1.5rem' }}
-            />
-          }
-          {decliningStatus === 1 &&
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              <CountdownCircleTimer
-                size={60}
-                strokeWidth={6}
-                isPlaying={decliningStatus === 1}
-                duration={UNDO_LIMIT}
-                colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
-                onComplete={handleDeclineAction}
-              >
-                {renderTime}
-              </CountdownCircleTimer>
-              <Typography variant="body" sx={{ ml: 3 }}>
-                You can undo this request in the remaining time.
-              </Typography>
-            </Box>
-          }
-          {decliningStatus === 2 &&
-            <div dangerouslySetInnerHTML={{
-              __html:
-                `The decline request has been sent.<br/><b>${currentParams.requesterName}</b> to <b>${currentParams.targetName}</b>`
-              }}
-              style={{ lineHeight: '1.5rem' }}
-            />
-          }
-        </DialogContent>
-        <DialogActions>
-          <Button
-            variant="contained"
-            type="submit"
-            onClick={handleDeclineAction}
-            disabled={decliningStatus === 2}
-          >
-            {decliningStatus === 1 ? 'Proceed' : 'Ok'}
-          </Button>
-          {decliningStatus === 1 && (
-            <Button
-              variant="contained"
-              type="submit"
-              onClick={handleUndoDeclineAction}
-              disabled={decliningStatus !== 1}
-            >
-              Cancel
-            </Button>
-          )}
-          <Button onClick={handleCloseDeclineModal}>Close</Button>
-        </DialogActions>
-      </Dialog>
-      <Dialog open={openGrantModal} onClose={handleCloseGrantModal} fullWidth={true} maxWidth={'sm'}>
-        <DialogTitle>Granting Request Modal</DialogTitle>
-        <DialogContent dividers>
-          {grantingStatus === 0 &&
-            <div
-              dangerouslySetInnerHTML={{
-              __html:
-                `Are you sure you want to grant access to <b>${currentParams.requesterName}</b> to <br/><b>${currentParams.targetName}</b>?`
-              }}
-              style={{ lineHeight: '1.5rem' }}
-            />
-          }
-          {grantingStatus === 1 &&
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              <CountdownCircleTimer
-                size={60}
-                strokeWidth={6}
-                isPlaying={grantingStatus === 1}
-                duration={UNDO_LIMIT}
-                colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
-                onComplete={handleGrantAction}
-              >
-                {renderTime}
-              </CountdownCircleTimer>
-              <Typography variant="body" sx={{ ml: 3 }}>
-                You can undo this request in the remaining time.
-              </Typography>
-            </Box>
-          }
-          {grantingStatus === 2 &&
-            <div dangerouslySetInnerHTML={{
-              __html:
-                `The grant request has been sent.<br/><b>${currentParams.requesterName}</b> to <b>${currentParams.targetName}</b>`
-              }}
-              style={{ lineHeight: '1.5rem' }}
-            />
-          }
-        </DialogContent>
-        <DialogActions>
-          <Button
-            variant="contained"
-            type="submit"
-            onClick={handleGrantAction}
-            disabled={grantingStatus === 2}
-          >
-            {grantingStatus === 1 ? 'Proceed' : 'Ok'}
-          </Button>
-          {grantingStatus === 1 && (
-            <Button
-              variant="contained"
-              type="submit"
-              onClick={handleUndoGrantAction}
-              disabled={grantingStatus !== 1}
-            >
-              Cancel
-            </Button>
-          )}
-          <Button onClick={handleCloseGrantModal}>Close</Button>
-        </DialogActions>
-      </Dialog>
+      <ActionModal
+        isOpenModal={openDeclineModal}
+        modalTitle="Declining Request"
+        questionMessage={`Are you sure you want to decline access to <b>${currentParams.targetName}</b> to <br/><b>${currentParams.targetName}</b>?`}
+        successMessage={`<b>${currentParams.targetName}</b><br />The create request has been sent.`}
+        handleDispatch={handleDeclineAction}
+        onCloseModal={handleCloseDeclineModal}
+      />
+      <ActionModal
+        isOpenModal={openGrantModal}
+        modalTitle="Granting Request"
+        questionMessage={`Are you sure you want to grant access to <b>${currentParams.targetName}</b> to <br/><b>${currentParams.targetName}</b>?`}
+        successMessage={`<b>${currentParams.targetName}</b><br />The create request has been sent.`}
+        handleDispatch={handleGrantAction}
+        onCloseModal={handleCloseGrantModal}
+      />
     </Container>
   );
 }
